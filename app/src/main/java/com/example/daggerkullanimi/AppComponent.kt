2003e172921797467440f8853a128c5c0e3062c7
DevAyhan <105974668+DevAyhan@users.noreply.github.com>
2022-08-28 09:24:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/daggerkullanimi/AppComponent.kt
@@ -0,0 +1,11 @@
+package com.example.daggerkullanimi
+
+import dagger.Component
+
+@Component (modules = [AppModule::class])
+interface AppComponent {
+
+    fun inject(activity: MainActivity)
+
+
+}
